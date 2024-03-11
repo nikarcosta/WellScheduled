19723c629316f125ff788dcaf63197747e3c3e0c
nikarcosta <mrc32467098@outlook.com>
2024-03-11 08:17:40 -0300

    set up project

---
+++ README.md
@@ -0,0 +1,5 @@
+# WellScheduled API Documentation
+
+Back-end for WellScheduled, a platform for scheduling medical appointments
+
+Coming soon!
---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const server = express();
+server.use(cors());
+server.use(json());
+
+const PORT = process.env.PORT;
+
+server.listen(PORT, () => {
+  console.log(`Server running at PORT = ${PORT}`);
+});
